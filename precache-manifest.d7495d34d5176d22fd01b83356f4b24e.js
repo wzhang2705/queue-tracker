b132e57546fc006384dec88b9c02c0ade4de611a
Wen Zhang <wz2705@gmail.com>
2020-09-05 17:30:18 -0400

    Updates

---
+++ precache-manifest.d7495d34d5176d22fd01b83356f4b24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63d2d620c8d23940fa07f5627e54f37",
+    "url": "/queue-tracker/index.html"
+  },
+  {
+    "revision": "84a8642108965e4639e3",
+    "url": "/queue-tracker/static/css/main.4ce8577e.chunk.css"
+  },
+  {
+    "revision": "c1f3215489631bd408ea",
+    "url": "/queue-tracker/static/js/2.a3d99cf0.chunk.js"
+  },
+  {
+    "revision": "da6f744259c153b4aae4fe0f3e9054cd",
+    "url": "/queue-tracker/static/js/2.a3d99cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a8642108965e4639e3",
+    "url": "/queue-tracker/static/js/main.a639012e.chunk.js"
+  },
+  {
+    "revision": "2e4b19108fd5240bde01",
+    "url": "/queue-tracker/static/js/runtime-main.e5b57128.js"
+  }
+]);
